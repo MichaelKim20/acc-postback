51214aad20c2007de117b133540708c62a9d060c
Michael Kim <michael.kim.200518@gmail.com>
2025-01-23 10:24:48 +0900

    Modify the user ID to separate and process phone numbers

--- src/routers/DefaultRouter.ts
+++ src/routers/DefaultRouter.ts
@@ -27,14 +27,6 @@ export class DefaultRouter {
         return this._web_service.app;
     }
 
-    private makeResponseData(code: number, data: any, error?: any): any {
-        return {
-            code,
-            data,
-            error,
-        };
-    }
-
     public registerRoutes() {
         this.app.get("/", [], this.getHealthStatus.bind(this));
         this.app.get(
@@ -42,13 +34,39 @@ export class DefaultRouter {
             [
                 query("postback_id").exists(),
                 query("event_name").exists(),
-                query("user_id").exists(),
+                query("user_id").exists().trim().isEthereumAddress(),
                 query("payout").exists(),
                 query("user_payout").exists(),
                 query("user_payout_in_vc").exists(),
                 query("publisher").exists(),
             ],
-            this.handler.bind(this)
+            this.handler_address.bind(this)
+        );
+        this.app.get(
+            "/handler/address",
+            [
+                query("postback_id").exists(),
+                query("event_name").exists(),
+                query("user_id").exists().trim().isEthereumAddress(),
+                query("payout").exists(),
+                query("user_payout").exists(),
+                query("user_payout_in_vc").exists(),
+                query("publisher").exists(),
+            ],
+            this.handler_address.bind(this)
+        );
+        this.app.get(
+            "/handler/phone",
+            [
+                query("postback_id").exists(),
+                query("event_name").exists(),
+                query("user_id").exists().trim().matches("^\\+?[0-9]+([ -][0-9]+)*$"),
+                query("payout").exists(),
+                query("user_payout").exists(),
+                query("user_payout_in_vc").exists(),
+                query("publisher").exists(),
+            ],
+            this.handler_phone.bind(this)
         );
         this.app.get("/metrics", [], this.getMetrics.bind(this));
     }
@@ -67,63 +85,117 @@ export class DefaultRouter {
         res.end(await this._metrics.metrics());
     }
 
-    private isValidETHAddress(str: string): boolean {
-        const regex = new RegExp(/^(0x)?[0-9a-fA-F]{40}$/);
-        if (str == null) return false;
-        return regex.test(str);
-    }
-
-    private async handler(req: express.Request, res: express.Response) {
+    private async handler_address(req: express.Request, res: express.Response) {
         let ip = req.get("X-Forwarded-For");
         if (ip === undefined) ip = req.connection.remoteAddress || "";
         if (!this._config.setting.whiteList.includes(ip)) {
-            this._metrics.add("failure", 1);
-            return res.status(400).json(
-                this.makeResponseData(400, undefined, {
-                    message: `Unauthorized IP - ${ip}`,
-                })
-            );
+            logger.error(`GET /handler/address: Unauthorized IP: ${ip}`);
+            return res.status(400).json({
+                status: "failure",
+                message: `Unauthorized IP - ${ip}`,
+                data: req.query,
+            });
         }
 
-        logger.http(`GET /handler ${ip}:${JSON.stringify(req.query)}`);
+        logger.http(`GET /handler/address ${ip}:${JSON.stringify(req.query)}`);
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json(
-                this.makeResponseData(400, undefined, {
-                    message: "Invalid parameters",
-                    validation: errors.array(),
-                })
-            );
+            logger.error(`GET /handler/address: Invalid parameters`);
+            return res.status(400).json({
+                status: "failure",
+                message: "Invalid parameters",
+                data: req.query,
+                validation: errors.array(),
+            });
         }
 
         try {
+            const user_id: string = String(req.query.user_id).trim();
+            const user_id_type: number = 0;
             const postback_id: string = String(req.query.postback_id).trim();
             const event_name: string = String(req.query.event_name).trim();
-            let user_id: string = String(req.query.user_id).trim();
             const payout: number = Number(req.query.payout);
             const user_payout: number = Number(req.query.user_payout);
             const user_payout_in_vc: number = Number(req.query.user_payout_in_vc);
             const publisher: string = String(req.query.publisher).trim();
-            let user_id_type: number;
-            if (this.isValidETHAddress(user_id)) {
-                user_id_type = 0;
+
+            await this._storage.saveItem(
+                postback_id,
+                event_name,
+                user_id,
+                user_id_type,
+                payout,
+                user_payout,
+                user_payout_in_vc,
+                publisher
+            );
+            return res.status(200).json({
+                status: "success",
+                message: "Request processed successfully",
+                data: req.query,
+            });
+        } catch (error: any) {
+            logger.error(`GET /handler/address: ${error.message}`);
+            this._metrics.add("failure", 1);
+            return res.status(500).json({
+                status: "failure",
+                message: error.message,
+                data: req.query,
+            });
+        } finally {
+            ///
+        }
+    }
+
+    private async handler_phone(req: express.Request, res: express.Response) {
+        let ip = req.get("X-Forwarded-For");
+        if (ip === undefined) ip = req.connection.remoteAddress || "";
+        if (!this._config.setting.whiteList.includes(ip)) {
+            logger.error(`GET /handler/phone: Unauthorized IP: ${ip}`);
+            return res.status(400).json({
+                status: "failure",
+                message: `Unauthorized IP - ${ip}`,
+                data: req.query,
+            });
+        }
+
+        logger.http(`GET /handler/phone ${ip}:${JSON.stringify(req.query)}`);
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            logger.error(`GET /handler/phone: Invalid parameters: ${JSON.stringify(errors.array())}`);
+            return res.status(400).json({
+                status: "failure",
+                message: "Invalid parameters",
+                data: req.query,
+                validation: errors.array(),
+            });
+        }
+
+        try {
+            let user_id: string = String(req.query.user_id).trim();
+            const user_id_type: number = 1;
+            const postback_id: string = String(req.query.postback_id).trim();
+            const event_name: string = String(req.query.event_name).trim();
+            const payout: number = Number(req.query.payout);
+            const user_payout: number = Number(req.query.user_payout);
+            const user_payout_in_vc: number = Number(req.query.user_payout_in_vc);
+            const publisher: string = String(req.query.publisher).trim();
+
+            if (user_id.substring(0, 1) !== "+") user_id = "+" + user_id;
+            const phoneUtil = PhoneNumberUtil.getInstance();
+            const number = phoneUtil.parseAndKeepRawInput(user_id, "ZZ");
+            if (phoneUtil.isValidNumber(number)) {
+                user_id = phoneUtil.format(number, PhoneNumberFormat.INTERNATIONAL);
             } else {
-                user_id_type = 1;
-                if (user_id.substring(0, 1) !== "+") user_id = "+" + user_id;
-                const phoneUtil = PhoneNumberUtil.getInstance();
-                const number = phoneUtil.parseAndKeepRawInput(user_id, "ZZ");
-                if (phoneUtil.isValidNumber(number)) {
-                    user_id = phoneUtil.format(number, PhoneNumberFormat.INTERNATIONAL);
-                } else {
-                    logger.error(`Invalid phone number format: ${user_id}`);
-                    this._metrics.add("failure", 1);
-                    return res.status(200).json(
-                        this.makeResponseData(500, undefined, {
-                            message: "Invalid phone number format",
-                        })
-                    );
-                }
+                logger.error(`GET /handler/phone: Invalid phone number format: ${user_id}`);
+                this._metrics.add("failure", 1);
+                return res.status(400).json({
+                    status: "failure",
+                    message: "Invalid parameters",
+                    data: req.query,
+                });
             }
 
             await this._storage.saveItem(
@@ -136,23 +208,19 @@ export class DefaultRouter {
                 user_payout_in_vc,
                 publisher
             );
-            return res
-                .status(200)
-                .json(
-                    this.makeResponseData(
-                        200,
-                        { postback_id, event_name, user_id, payout, user_payout, user_payout_in_vc, publisher },
-                        null
-                    )
-                );
+            return res.status(200).json({
+                status: "success",
+                message: "Request processed successfully",
+                data: req.query,
+            });
         } catch (error: any) {
-            logger.error(`GET /handler : ${error.message}`);
+            logger.error(`GET /handler/phone: ${error.message}`);
             this._metrics.add("failure", 1);
-            return res.status(200).json(
-                this.makeResponseData(500, undefined, {
-                    message: error.message,
-                })
-            );
+            return res.status(500).json({
+                status: "failure",
+                message: error.message,
+                data: req.query,
+            });
         } finally {
             ///
         }
